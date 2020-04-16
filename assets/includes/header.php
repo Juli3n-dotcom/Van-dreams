3f583baa43c93bd4f223776b0ad36935b1a2e266
Julien QUENTIER <j.quentier@gmail.com>
2020-04-16 11:57:06 +0200

    Création du footer

---
+++ assets/includes/header.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?=$page_title?> | Van Dreams </title>
+    <!--Ion Icons-->
+    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+    <!--Google Fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Aldrich&display=swap" rel="stylesheet">
+    <!--Our own stylesheet-->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<header>
+    <div class=container>
+        <nav>
+            <div class="nav-brand">
+        <a href="index.html">
+            <img src="assets/img/logo_1.png" alt="">
+        </a>
+    </div>
+
+    <div class="menu-icons open" >
+        <i class="icon ion-md-menu"></i>
+    </div>
+
+    <ul class="nav-list">
+        <div class="menu-icons close">
+            <i class="icon ion-md-close"></i>
+        </div>
+        <li class="nav-item">
+            <a href="Home" class="nav-link current">Home</a>
+        </li>
+        <li class="nav-item">
+            <a href="Home" class="nav-link">Destination</a>
+        </li>
+        <li class="nav-item">
+            <a href="Home" class="nav-link">Pricing</a>
+        </li>
+        <li class="nav-item">
+            <a href="Home" class="nav-link">Booking</a>
+        </li>
+        <li class="nav-item">
+            <a href="Home" class="nav-link">About</a>
+        </li>
+    </ul>
+</nav>
+</div>
+</header>
+
+<main>
