a0814a87d5b6448edd544db0341f2fc68b1035ce
Julien QUENTIER <j.quentier@gmail.com>
2020-04-16 15:23:22 +0200

    Création du hero

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?=$page_title?> | Van Dreams </title>
+    <link rel="icon" href="assets/img/logo_1.png">
     <!--Ion Icons-->
     <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
     <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
--- index.php
+++ index.php
@@ -1,7 +1,25 @@
 <?php
+$page_title ='Accueil';
 include __DIR__.'/assets/includes/header.php';
 ?>
 
+<section class="hero">
+        <div class="container">
+            <div class="main-message">
+                <h3>The Great Outdoor</h3>
+                <H1>Adventure</H1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Perspiciatis dolore quidem sit aut quo illum repellendus unde 
+                    beatae pariatur suscipit, recusandae delectus maxime veritatis
+                     mollitia ipsam saepe odit? Accusantium, eius.
+                </p>
+                <div class="cta">
+                    <a href="" class="btn">Book Now</a>
+                </div>
+            </div>
+        </div>
+    </section>
 
 <?php
 include __DIR__.'/assets/includes/footer.php';
