2a15d4b83d2baa7013cef1a4def3cd72ef10cc88
Julien QUENTIER <j.quentier@gmail.com>
2020-04-19 12:12:09 +0200

    Correction mineur

--- admin/category.php
+++ admin/category.php
@@ -117,7 +117,7 @@ include __DIR__.'/assets/includes/header_admin.php';
                             <div class="modal-dialog modal-dialog-scrollable" role="document">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalScrollableTitle">Modification Catégorie | <?= $cat['titre']??'';?></h5>
+                                        <h5 class="modal-title" >Modification Catégorie | <?= $cat['titre']??'';?></h5>
                                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                         </button>
@@ -149,7 +149,7 @@ include __DIR__.'/assets/includes/header_admin.php';
                             <div class="modal-dialog modal-dialog-scrollable" role="document">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalScrollableTitle">Supprimer Catégorie | <?= $cat['titre']??'';?></h5>
+                                        <h5 class="modal-title">Supprimer Catégorie | <?= $cat['titre']??'';?></h5>
                                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                         </button>
