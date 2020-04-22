ee95e9713265a06b48ba8e4a370c0df63b794af3
Julien QUENTIER <j.quentier@gmail.com>
2020-04-22 19:37:02 +0200

    déconnexion auto

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,35 @@ const title = document.querySelector('.nav_title');
       hamburger.classList.remove('active');
    }
 })
+
+$(function()
+{
+   function timeChecker()
+      {
+        setInterval(function(){
+         var storedTimeStamp = sessionStorage.getItem("lastTimeStamp");
+         timeCompare(storedTimeStamp);
+        },3000);
+      }
+
+      function timeCompare(timeString){
+         var currentTime   = new Date();
+         var pastTime      = new Date(timeString);
+         var timeDiff      = currentTime - pastTime;
+         var minPast       = Math.floor( (timeDiff/60000) );
+
+         if (minPast > 5){
+            sessionStorage.removeItem('lastTimeStamp');
+            window.location = "logout.php";
+            return false
+         }
+         
+      }
+   
+   $(document).mousemove(function(){
+      var timeStamp = new Date();
+      sessionStorage.setItem('lastTimeStamp',timeStamp);
+   });
+
+   timeChecker()
+});
