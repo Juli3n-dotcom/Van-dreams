05ea4b603d6abba6076b057d30ad4cf0cf9c2021
Julien QUENTIER <j.quentier@gmail.com>
2020-04-24 19:02:19 +0200

    New hero

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -33,7 +33,7 @@ require_once __DIR__ . '/../config/bootstrap.php';
 </head>
 <body>
 <header>
-    <div class=container>
+    <div class=container_header>
         <nav>
             <div class="nav-brand">
         <a href="index.php">
--- index.php
+++ index.php
@@ -7,17 +7,18 @@ include __DIR__.'/assets/includes/header.php';
 <?php include __DIR__.'/assets/includes/flash.php';?>
 
 <section class="hero">
-        <div class="container">
-            <div class="main-message">
-                <H1>Van Dreams</H1>
-                <p>
-                le site de petites annonces DE TRIPPEURS à TRIPPEURS. Consultez des milliers d'annonces de van aménagé
-                </p>
-                <div class="cta">
-                    <a href="" class="btn">Trouve le tien</a>
-                </div>
-            </div>
-        </div>
+        
+            
+        <p class="coords">N 49° 16' 35.173" / W 0° 42' 11.30"</p>
+
+<div class="ellipse-container">
+    <h2 class="greeting">Van Dreams</h2>
+    <div class="ellipse ellipse__outer--thin">
+        <div class="ellipse ellipse__orbit"></div>
+    </div>
+    <div class="ellipse ellipse__outer--thick"></div>
+           
+        
     </section>
 
 <?php
