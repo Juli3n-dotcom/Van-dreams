065888946193b407eac127b32b0b3b023cee5e29
Julien QUENTIER <j.quentier@gmail.com>
2020-05-09 16:46:50 +0200

    Sup doublon

--- sql/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-setcookie('token','',time()-3600);
-require_once __DIR__ . '/assets/config/bootstrap.php';
-unset($_SESSION['membre']);
-ajouterFlash('success','Vous avez bien été déconnecté');
-header('Location: index.php');
-
