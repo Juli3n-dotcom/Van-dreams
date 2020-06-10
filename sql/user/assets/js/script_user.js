2dd589c2beccdf81c9686615ba029fadf82a8bae
Julien QUENTIER <j.quentier@gmail.com>
2020-06-10 19:07:09 +0200

    suppression de doublons

--- sql/user/assets/js/script_user.js
+++
@@ -1,9 +0,0 @@
-const nav = document.querySelector('.nav-list_user');
-
-document.querySelector('.open').addEventListener('click',()=>{
-   nav.classList.add('active');
-});
-
-document.querySelector('.close').addEventListener('click',()=>{
-   nav.classList.remove('active');
-});
