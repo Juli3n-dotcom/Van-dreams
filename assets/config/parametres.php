ca5b1e4c8203cae4ffd4e501e2c4ee36e29a9389
Julien QUENTIER <j.quentier@gmail.com>
2020-06-12 12:03:38 +0200

    Merge de master

--- assets/config/parametres.php
+++ assets/config/parametres.php
@@ -1,14 +1,14 @@
 <?php
 
-//  const DB_HOST = 'localhost';
-//  const DB_NAME = 'van_dream';
-//  const DB_USER = 'root';
-//  const DB_PASS = 'root';
+ const DB_HOST = 'localhost';
+ const DB_NAME = 'van_dream';
+ const DB_USER = 'root';
+ const DB_PASS = 'root';
 
-const DB_HOST = 'sql25';
- const DB_NAME = 'swr60305';
- const DB_USER = 'swr60305';
- const DB_PASS = 'E05QA8Z7blR0';
+// const DB_HOST = 'sql25';
+//  const DB_NAME = 'swr60305';
+//  const DB_USER = 'swr60305';
+//  const DB_PASS = 'E05QA8Z7blR0';
 
  const ROLE_USER = 0;
  const ROLE_ADMIN = 1;
